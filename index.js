fc166ed9a70de5da7185cdef3d741b857779a7ce
Nochilas <patrononicholas@gmail.com>
2022-01-17 18:26:30 +0100

    First Commit

---
+++ index.js
@@ -0,0 +1,33 @@
+//Get the id of the clicked button
+$(".dice").click(function(e){
+    //Call function that generates a random number
+    RandomNum(e);
+});
+
+//Make custom options visible
+$(".custom").click(function(){
+    $("#ndice").css("visibility", "visible");
+    $(".rollcustom").css("visibility", "visible");
+
+    $(".rollcustom").click(function(){
+        var customsides = document.querySelector("#ndice").value;
+        console.log(customsides);
+        var num = Math.floor(Math.random() * customsides + 1);
+        $("#result").attr("value", num);
+    });
+});
+
+//Function to reset all dice rolls
+$(".reset").click(function(){
+    $("#ndice").css("visibility", "hidden");
+    $(".rollcustom").css("visibility", "hidden");
+    $("#result").attr("value", 0);
+});
+
+//Function to generate a random number
+function RandomNum(e){
+    var sides = e.target.id;
+    
+    var num = Math.floor(Math.random() * sides + 1);
+    $("#result").attr("value", num);
+}
